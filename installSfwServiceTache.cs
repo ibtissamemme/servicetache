464939095c1736478ba1ef3b08a23fe5cd665542
Mathias TISSEUIL <mathias.tisseuil@safeware.fr>
2019-01-04 16:04:16 +0100

    gitirnore updated

---
+++ installSfwServiceTache.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+
+
+namespace sfwServiceTache
+{
+    [RunInstaller(true)]
+    public partial class installSfwServiceTache : Installer
+    {
+        public installSfwServiceTache()
+        {
+            InitializeComponent();
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+
+            // 
+            // serviceInstaller1
+            // 
+            this.serviceInstaller1.Description = "Appel au web service ws_importation";
+            this.serviceInstaller1.DisplayName = "sfwServiceTache";
+            this.serviceInstaller1.ServiceName = "sfwServiceTache";
+            this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.serviceInstaller1});
+        }
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller1;
+    }
+}
